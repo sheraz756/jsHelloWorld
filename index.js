3b6af742f537c49c8220137145e5b1270607f7c1
sheraz <sherazzafar148@gmail.com>
2023-05-27 13:01:29 +0500

    ok

---
+++ index.js
@@ -0,0 +1,77 @@
+//alert("hello world")
+// var name = "sheraz";
+// var location = "karachi"
+// let Name = "Sheraz";
+// alert(Name)
+// let 1sheraz = "sheraz" //illegal 
+
+// let Sheraz = "abc"// legal
+
+// let @sheraz = '' illega;
+
+// let $var = ''
+
+// let alert = 'sheraz'
+// for assign a variable  that contain all of these Small letter capital letter $ _ number
+
+
+
+// alert(alert)
+
+//let Name = obMarks*100/totalMarks
+
+
+
+// let english = (parseFloat(prompt('enter your number')))
+// let urdu = (parseFloat(prompt('enter your number')))
+// let maths = (parseFloat(prompt('enter your number')))
+
+
+// let obtainedMarks = english+urdu+maths
+
+
+//operators
+
+// +-*/
+
+
+
+// const mynameissheraz = '1'
+
+// const myameIsSheraz = '1'
+// let firstNumber = 10;
+// let secondNumber = 20;
+
+// console.log(a-b)
+
+// let userInput = (parseFloat(prompt('enter your number')))
+// let userInput1 = (parseFloat(prompt('enter your number')))
+
+// console.log(userInput+userInput1)
+
+
+
+
+
+
+
+
+
+
+
+// console.log(numberOne)
+
+
+// control Flow
+//
+let NumberOne = (parseFloat(prompt('enter your number')))
+let NumberTwo = (parseFloat(prompt('enter your number')))
+// let maths = (parseFloat(prompt('enter your number')))
+
+if(NumberOne >NumberTwo )
+{
+    console.log('number is greateer then to NumberTwo')
+}
+else{
+    console.log('number is less then to NumberTwo')
+}
